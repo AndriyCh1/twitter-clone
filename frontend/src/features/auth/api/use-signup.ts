242e0078a33178920d60e337ae32bdb17f8b71ec
AndriyCh1 <chekanovskiyacc@gmail.com>
2024-07-03 03:15:05 +0300

    fix: redirect after successful signup

--- frontend/src/features/auth/api/use-signup.ts
+++ frontend/src/features/auth/api/use-signup.ts
@@ -1,9 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { ISignUpPayload } from "../types";
 import { authService } from "./auth-service";
+import { authKeys } from "./auth-keys";
 
 export const useSignUp = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (data: ISignUpPayload) => authService.signup(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: authKeys.authUser() });
+    },
   });
 };
